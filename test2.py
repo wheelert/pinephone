c8b9e8f7206cd0cfefea25cbced6795297529e81
Thomas Wheeler <thomas@wheelerwire.com>
2021-03-12 21:38:41 -0800

    tests

--- test2.py
+++ test2.py
@@ -6,14 +6,25 @@ from gi.repository import Gtk
 
 class MyWindow(Gtk.Window):
     def __init__(self):
-        Gtk.Window.__init__(self, title="Hello World")
+        Gtk.Window.__init__(self, title="Custom Commands")
 
-        self.button = Gtk.Button(label="Click Here")
+        self.button = Gtk.Button(label="Enable Data")
         self.button.connect("clicked", self.on_button_clicked)
         self.add(self.button)
+        
+        #exit button
+        self.button2 = Gtk.Button(label="Exit")
+        self.button2.connect("clicked", self.AppClose)
+        self.add(self.button2)
+        
+        
+        
 
     def on_button_clicked(self, widget):
         print("Hello World")
+        
+    def AppClose(self, widget):
+        exit()
 
 
 win = MyWindow()
